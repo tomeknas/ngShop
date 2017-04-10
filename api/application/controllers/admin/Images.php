1db9b11c20bfe56a8c1ced4e38f772fe2c0ce9ce
tomeknas <tomeknas@o2.pl>
2017-04-10 21:23:59 +0200

    Paste upload function

--- api/application/controllers/admin/Images.php
+++ api/application/controllers/admin/Images.php
@@ -6,6 +6,17 @@ class Images extends CI_Controller {
 	
 	public function upload()
 	{
-		echo 'Users...';
-	}
+			if ( !empty( $_FILES ) ) {
+	    $tempPath = $_FILES[ 'file' ][ 'tmp_name' ];
+	    $uploadPath = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . $_FILES[ 'file' ][ 'name' ];
+	    move_uploaded_file( $tempPath, $uploadPath );
+	    $answer = array( 'answer' => 'File transfer completed' );
+	    $json = json_encode( $answer );
+	    echo $json;
+		} 
+		else 
+		{
+		    echo 'No files';
+		}
+			}
 }
