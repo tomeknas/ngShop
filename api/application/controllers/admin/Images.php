7a118236f012b61114ed50fca5e8ace103a3f692
tomeknas <tomeknas@o2.pl>
2017-04-18 11:34:18 +0200

    Create function to delete images

--- api/application/controllers/admin/Images.php
+++ api/application/controllers/admin/Images.php
@@ -42,7 +42,20 @@ class Images extends CI_Controller {
 	    			$newFiles[] .= $file;
 	    		}
 
-	    		echo json_encode($newFiles);
+	    		echo json_encode($newFiles); 
+			}
+
+			public function delete(){
+				$post = file_get_contents('php://input');
+				$_POST= json_decode($post, true);
+				
+				$id = $this->input->post('id');
+				$image = $this->input->post('image');
+
+				$imagePath = FCPATH . '..' . DIRECTORY_SEPARATOR . 'uploads'. DIRECTORY_SEPARATOR;
+	    		$imagePath = $imagePath . $id . DIRECTORY_SEPARATOR;
+	    		$imagePath = $imagePath . $image;
+	    		unlink($imagePath);
 			}
 }
 
