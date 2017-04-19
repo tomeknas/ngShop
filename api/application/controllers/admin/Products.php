0bd37882d7eda62b45f3b22d5626298c233dd3b7
tomeknas <tomeknas@o2.pl>
2017-04-19 19:05:02 +0200

    Function update

--- api/application/controllers/admin/Products.php
+++ api/application/controllers/admin/Products.php
@@ -7,16 +7,24 @@ class Products extends CI_Controller {
 		parent::__construct();
 
 		$post = file_get_contents( 'php://input' );
-		$_POST = json_decode( $post , true );		
+		$_POST = json_decode( $post , true );
+		$this->load->model('admin/Products_model');		
 
 	}
 
 	public function get( $id = false )
 	{
 
-		$this->load->model('admin/Products_model');
+		
 		$output = $this->Products_model->get($id);
 		echo json_encode($output);
+	}
+
+	public function update()
+	{
+		$product = $this->input->post('product');
+		$this->Products_model->update($product);
+		
 	}
 
 }
